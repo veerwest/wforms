8803dbb341b348951377db007def9354fb25eb5a
matt@haralovich.org <matt@haralovich.org@709aefc4-6f42-0410-a42d-23c4347884e8>
2008-06-05 16:22:14 +0000

    * Updated base2. * Created a crude build script that works in linux. * Updated the wforms build.

---
+++ tools/build.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from jsmin import jsmin
+
+branch = os.path.abspath(os.path.dirname(__file__) +'/..')
+
+modules = [
+	'lib/src/base2.js',
+	'lib/src/base2-dom.js',
+	'wforms_core.js',
+	'wforms_hint.js',
+	'wforms_paging.js',
+	'wforms_repeat.js',
+	'wforms_switch.js',
+	'wforms_validation.js',
+	'wforms_calculation.js'
+]
+
+compilation = []
+for module in modules:
+	f = open(branch +'/'+ module)
+	compilation.append(f.read())
+	f.close()
+
+wforms = open(branch +'/build/wforms.js', 'w')
+print 'Writing '+ wforms.name
+wforms.write(jsmin("\n\n".join(compilation)))
+wforms.close()
