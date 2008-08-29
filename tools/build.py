e1d33d34396f86c861b3c13fd0ce881caa9850b6
matt@haralovich.org <matt@haralovich.org@709aefc4-6f42-0410-a42d-23c4347884e8>
2008-08-29 19:49:25 +0000

    Updated build.py to include license information.

--- tools/build.py
+++ tools/build.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
-import os
+import os, re
 
 branch = os.path.abspath(os.path.dirname(__file__) +'/..')
 
-modules = [
-	'lib/src/base2.js',
-	'lib/src/base2-dom.js',
+head = '''/* wForms %s
+A javascript extension to web forms.
+
+Copyright (c) 2005-2007 Cedric Savarese <cedric@veerwest.com> and contributors.
+This software is licensed under the CC-GNU LGPL <http://creativecommons.org/licenses/LGPL/2.1/>
+For more information, visit: http://www.formassembly.com/wForms
+
+wForms version 3.0 by Demid Nikitin and Cedric Savarese.
+wForms 3.0 requires base2 - copyright 2007, Dean Edwards. */'''
+
+modules = (
+	'lib/base2-p.js',
+	'lib/base2-dom-p.js',
 	'wforms_core.js',
 	'wforms_hint.js',
 	'wforms_paging.js',
@@ -14,13 +24,16 @@ modules = [
 	'wforms_switch.js',
 	'wforms_validation.js',
 	'wforms_calculation.js'
-]
+)
 
 compilation = []
 for module in modules:
 	f = open(branch +'/'+ module)
-	compilation.append(f.read())
+	compilation.append(f.read().strip())
 	f.close()
+
+m = re.search('wFORMS\.VERSION\s+=\s+"([^"]+)";', compilation[2])
+compilation = [head % m.group(1)] + compilation
 
 wforms = open(branch +'/build/wforms.js', 'w')
 print 'Writing '+ wforms.name
