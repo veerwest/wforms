d23a854372059a6f51d1a142a75df39d6cc690f9
anchangsi@gmail.com <anchangsi@gmail.com@709aefc4-6f42-0410-a42d-23c4347884e8>
2011-10-13 18:18:36 +0000

    now validationFloat can deal with negative numbers correctly.

--- wforms_validation.js
+++ wforms_validation.js
@@ -601,7 +601,7 @@ wFORMS.behaviors.validation.instance.prototype.validateInteger = function(elemen
  * @returns {boolean} 
  */
 wFORMS.behaviors.validation.instance.prototype.validateFloat = function(element, value) {
-	var regexp = /^((([1-9]\d*|0)?\.\d+)|([1-9]\d*))$/;
+	var regexp = /^[\-+]?((([1-9]\d*|0)?\.\d+)|([1-9]\d*))$/;
 	return this.isEmpty(value) || regexp.test(value);
 }
 
